eca7567937e0ed734956a1fc8aeb2b3067443840
Ahmed Halim <ahmedhalim96@bitbucket.org>
2020-05-19 00:49:04 +0200

    Laravel Passport Setup

---
+++ database/seeds/seed_user_table.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class SeedUserTable extends Seeder {
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+
+  }
+}
